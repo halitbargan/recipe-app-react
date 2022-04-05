11c8187fb6e897d7c8d8049efc7e708a6654ab76
halitbargan <hltbrgn@gmail.com>
2022-04-05 03:46:10 +0300

    v1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
+
+// id= bc371d7b 
+// keys  = e020d4b08e7270a850c28fb6ca8b3fba	—
+
+
