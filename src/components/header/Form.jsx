f46480ca88be8bc4f62dd17590430a6eed47735c
halitbargan <hltbrgn@gmail.com>
2022-04-08 01:14:04 +0300

    v3

--- src/components/header/Form.jsx
+++ src/components/header/Form.jsx
@@ -2,13 +2,26 @@
 import React from 'react'
 import { FoodInput, FormContainer,Button,Select} from './HeaderStyles'
 
-const Form = () => {
+const Form = ({setQuery,setMeal,getData,mealTypes}) => {
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    getData();
+  }
   return (
-   <FormContainer> 
-     <FoodInput type="text" placeholder='Search'/>
+   <FormContainer onSubmit={handleSubmit}> 
+     <FoodInput type="text" placeholder='Search'
+     onChange={(e)=>setQuery(e.target.value)} />
      <Button type="submit" >Search</Button>
 
-     <Select name="mealTypes" id="mealTypes"></Select>
+     <Select 
+     name="mealTypes"
+     id="mealTypes" 
+     onChange={(e)=>setMeal(e.target.value)}>
+       {mealTypes.map((item,index)=>
+       <option value={item} key={index}>
+         {item}
+       </option>)}
+     </Select>
    </FormContainer>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,9 +8,10 @@ const APP_KEY= "e020d4b08e7270a850c28fb6ca8b3fba"
 
 const Home = () => {
   const [query, setQuery] = useState("")
+  const mealTypes=["Breakfast", "Lunch", "Dinner","Snack", "TeaTime"]
   const [meal, setMeal] = useState(mealTypes[0].toLowerCase())
   const [food, setFood] = useState();
-  const mealTypes=["Breakfast", "Lunch", "Dinner","Snack", "TeaTime"]
+  
   const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${meal}`;
 
   const getData= async ()=>{
