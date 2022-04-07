fb4f5eab6d4d67368bc4481bcbefc768c3a838d6
halitbargan <hltbrgn@gmail.com>
2022-04-08 00:50:57 +0300

    get API completed

--- src/components/header/Form.jsx
+++ src/components/header/Form.jsx
@@ -1,9 +1,15 @@
 
 import React from 'react'
+import { FoodInput, FormContainer,Button,Select} from './HeaderStyles'
 
 const Form = () => {
   return (
-    <div>Form</div>
+   <FormContainer> 
+     <FoodInput type="text" placeholder='Search'/>
+     <Button type="submit" >Search</Button>
+
+     <Select name="mealTypes" id="mealTypes"></Select>
+   </FormContainer>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,36 @@
+import React,{useState} from 'react'
+import Header from "../../components/header/Header"
+import axios from "axios"
 
-import React from 'react'
+
+const APP_ID="bc371d7b"
+const APP_KEY= "e020d4b08e7270a850c28fb6ca8b3fba"
 
 const Home = () => {
+  const [query, setQuery] = useState("")
+  const [meal, setMeal] = useState(mealTypes[0].toLowerCase())
+  const [food, setFood] = useState();
+  const mealTypes=["Breakfast", "Lunch", "Dinner","Snack", "TeaTime"]
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${meal}`;
+
+  const getData= async ()=>{
+    if(query) {
+    const result = await axios.get(url);
+    console.log(result);
+    setFood(result.data.hits)
+  } else {
+    console.log("please fill the form");
+  }
+  }
+
   return (
-    <div>Home</div>
+    <div>
+      <Header setQuery={setQuery}
+      getData={getData}
+      mealTypes={mealTypes}
+      setMeal={setMeal}/>
+
+    </div>
   )
 }
 
